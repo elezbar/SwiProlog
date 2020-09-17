85f3dab5d9a41803088baa5976eea7fe65399a5d
elezbar <elezbar000@gmail.com>
2020-09-17 05:00:00 +0300

    Добавление второго предиката.

--- int2.pl
+++ int2.pl
@@ -1,2 +1,7 @@
 inList([H|_],B):-B is H, true.
 inList([_|T],B):-inList(T,B).
+
+allDel(A,X):-allDel(A,[],2,X).
+allDel(A,Del,I,Del):- I is A div 2 + 1 .
+allDel(A,Del,I,X):- F is A mod I, F is 0, I1 is I+1, allDel(A,[I|Del],I1,X).
+allDel(A,Del,I,X):- I1 is I+1, allDel(A,Del,I1,X).
